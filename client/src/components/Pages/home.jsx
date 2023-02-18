1c9a4b10cc15e523dc4ca6ee5e6abc5cf3eaf426
KishanPoddar <kishanpoddar2002@gmail.com>
2023-02-18 15:20:50 +0530

    made categories of bhajan responsive

--- client/src/components/Pages/home.jsx
+++ client/src/components/Pages/home.jsx
@@ -4,8 +4,8 @@ import GetBhajanByCategory from "../bhajans/getBhajanByCategory";
 const Home = () => {
     return (
         <>
-            <div className="h-screen flex mt-7">
-                <div className="h-[50rem] 2xl:ml-20 medium:ml-16 ml-14 mt-3 w-96">
+            <div className=" flex lg:flex-row flex-col lg:mt-7 mt-0">
+                <div className=" 2xl:ml-20 medium:ml-16 mt-3 lg:w-96 w-3/4 m-auto lg:m-0 ">
                     <GetBhajanByCategory />
                 </div>
                 <div className="h-full w-full px-5 ">
--- client/src/components/bhajans/getBhajanByCategory.jsx
+++ client/src/components/bhajans/getBhajanByCategory.jsx
@@ -1,31 +1,54 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { AiFillCaretDown, AiFillCaretUp } from "react-icons/ai";
 
 const GetBhajanByCategory = () => {
-    const [active, setActive] = useState(false)
-    const [key, setKey] = useState(-1)
-    const menuLinks = [
-        { name: "Shyam", link: "/" },
-        { name: "Shiv", link: "/" },
-        { name: "Durga", link: "/" },
-        { name: "Hanuman ", link: "/" },
-        { name: "Ganesh ", link: "/" },
-        { name: "Ramdev Baba ", link: "/" },
-        { name: "Krishna ", link: "/" },
-        { name: "Pitar Ji", link: "/" },
+    const [open, setOpen] = useState(false);
+    // const [activeBhajan, setActiveBhajan] = useState("second")
+    const categoryLinks = [
+        { name: "Shyam Bhajan", link: "/" },
+        { name: "Shiv Bhajan", link: "/" },
+        { name: "Durga Bhajan", link: "/" },
+        { name: "Hanuman Bhajan", link: "/" },
+        { name: "Ganesh Bhajan", link: "/" },
+        { name: "Ramdev Bhajan", link: "/" },
+        { name: "Krishna Bhajan", link: "/" },
+        { name: "Pitar Ji Bhajan", link: "/" },
         { name: "Chalisa", link: "/" },
         { name: "Aarti ", link: "/" },
     ];
     return (
-        <div>
-            <ul className="px-7 mt-5">
-                {menuLinks?.map((menu, i) => (
-                <li key={i} onClick={()=>{setActive(true); setKey(i)}} className={`borderBottom first-letter:uppercase `}> {/*borderBottom  ${active?"activeLink":""}*/}
-                    <Link to={menu?.link}  className={``} >{menu?.name}  Bhajan</Link>
-                </li>
-                ))}
-            </ul>
-        </div>
+        <>
+            <div className="block lg:hidden mb-6">
+                <button onClick={() => {setOpen(!open);}} className={`bg-red-400 px-2 py-1 w-full flex justify-between items-center font-bold ${open ? "rounded-t-lg" : "rounded-lg"} tracking-wider border-4 border-transparent  `}>
+                    Bhajans
+                    {!open ? (
+                        <AiFillCaretDown className="h-8" />
+                    ) : (
+                        <AiFillCaretUp className="h-8" />
+                    )}
+                </button>
+                <div className={`bg-red-300 lg:hidden relative font-medium ${open ? "block rounded-b-lg" : "hidden"}`}>
+                    <ul className="flex flex-col items-center">
+                        {categoryLinks?.map((category, i) => (
+                            <li onClick={() => setOpen(false)} key={i} className={`bhajanCategoryMobile`}>
+                                <Link to={category?.link}>{category?.name}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+
+            <div className="lg:block hidden">
+                <ul className="px-7 ">
+                    {categoryLinks?.map((category, i) => (
+                        <li key={i} className={`bhajanCategoryDesktop`}>
+                            <Link to={category?.link}>{category?.name}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
     );
 };
 
